73447e41a988eedba0e1cefc494221797b5916f0
= <=>
2023-04-10 11:08:33 +0900

    window/mac 호환테스트

--- src/controller/Auth.py
+++ src/controller/Auth.py
@@ -6,6 +6,8 @@ from src.utill.DBHelper import db_helper
 from src.process.AuthProcess import AuthProcess
 from src.utill.Decorator import *
 from src.evn.appEnvirment import *
+import random
+from src.utill.AES256 import AES256
 
 users = {}
         
@@ -46,9 +48,34 @@ class AuthLogin(Resource):
     
 			with db_helper.get_resource_rdb() as (cursor, _):
 					cursor.execute(sql, param)
-    
+     
+			## 랜덤 숫자 생성
+			randomNumber = random.randint(1000000000, 9999999999)
+   
+			## 랜덤숫자중 2,5,6,10번째 숫자를 |로 변경
+			kkk = str(randomNumber)
+			kkk = kkk[:1] + "&" + kkk[2:4] + "x" + kkk[5:6] + "q" + kkk[7:9] + "#" + kkk[10:]
+
+			## randomNumber 16자리로 맞추기
+			kkk = kkk + "l^2rq@aij121"
+			kkk = kkk[:16]
+   
+			## chip을 AES256으로 암호화
+			aes = AES256(kkk)
+   
+			en = aes.encrypt(password)
+   
+			## 암호화키, 마지막 로그인 저장
+			sql = "UPDATE employee SET chip_key = %s, last_login_date = now() WHERE idx = %s;"
+			param = (en, result['idx'])
+
+			with db_helper.get_resource_rdb() as (cursor, _):
+					cursor.execute(sql, param)
+       
 			return {
 				"result": True,
+				"disit": randomNumber,
+				"check": en,
 				'Authorization': jwt.encode( {"employee_idx": result["idx"], 'id': id, "name": result["name"], 'level':result["level"]}, JWT_PASSWORD, algorithm=JWT_ALGORITHMS) # str으로 반환하여 return
 			}
 
